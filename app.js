b291c179705e0b319f53ca7e65a79568cb71c507
Emily Landers <emily.j.landers@gmail.com>
2021-09-14 19:45:37 -0400

    about me js

---
+++ app.js
@@ -0,0 +1,42 @@
+let name = prompt('What is your name?');
+function getUserName(){
+    console.log(name)
+    }
+        document.write('Welcome ' + name)
+getUserName()
+
+let dog = prompt('Do I have a dog?');
+if (dog.toLowerCase() == ('yes')){
+alert("Correct!");
+}
+else{
+    alert("Incorrect!")
+}
+let food = prompt('Am I a vegetarian?');
+if (dog.toLowerCase() == ('yes')){
+alert("Correct!");
+}
+else{
+    alert("Incorrect!")
+}
+let potato = prompt('Did I eat 4 potatoes worth of french fries for dinner?');
+if (potato.toLowerCase() == ('yes')){
+alert("Correct!");
+}
+else{
+    alert("Incorrect!")
+}
+let cat = prompt('Do I have a cat?');
+if (cat.toLowerCase() == ('yes')){
+alert("Correct!");
+}
+else{
+    alert("Incorrect!")
+}
+let blonde = prompt('Is my hair naturally blonde?');
+if (blonde.toLowerCase() == ('no')){
+alert("Correct!");
+}
+else{
+    alert("Incorrect!")
+}
