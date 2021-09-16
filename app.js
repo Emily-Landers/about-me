3e58740d3c88624b8961aae7c4cbd95941c4af08
Emily Landers <emily.j.landers@gmail.com>
2021-09-15 20:33:30 -0400

    finished quiz for js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'; 
-
+let correctAnswers = 0
 let name = prompt('What is your name?');
 function getUserName(){
     //console.log(name)
@@ -10,6 +10,7 @@ getUserName()
 let dog = prompt('Do I have a dog?');
 if (dog.toLowerCase() == ('yes')){
 alert("Correct!");
+correctAnswers++; 
 }
 else{
     alert("Incorrect!")
@@ -19,6 +20,7 @@ else{
 let food = prompt('Am I a vegetarian?');
 if (food.toLowerCase() == ('yes')){
 alert("Correct!");
+correctAnswers++;
 }
 else{
     alert("Incorrect!")
@@ -28,6 +30,7 @@ else{
 let potato = prompt('Did I eat 4 potatoes worth of french fries for dinner?');
 if (potato.toLowerCase() == ('yes')){
 alert("Correct!");
+correctAnswers++;
 }
 else{
     alert("Incorrect!")
@@ -37,6 +40,7 @@ else{
 let cat = prompt('Do I have a cat?');
 if (cat.toLowerCase() == ('yes')){
 alert("Correct!");
+correctAnswers++;
 }
 else{
     alert("Incorrect!")
@@ -46,8 +50,63 @@ else{
 let blonde = prompt('Is my hair naturally blonde?');
 if (blonde.toLowerCase() == ('no')){
 alert("Correct!");
+correctAnswers++;
 }
 else{
     alert("Incorrect!")
 }
 //console.log(blonde)
+
+
+function game(){
+    let play = prompt('Do you want to play a game?');
+    let correctAnswer; 
+    if (play == 'yes'){
+    correctAnswer = Math.floor(Math.random() * 11);
+    }
+    if (play.toLowerCase() == 'yes'){
+        let numberOfAttempts = 4;
+        for(let i = 1; i <= numberOfAttempts; i++){
+            let answer = prompt("Guess a number between 1 and 10");
+            console.log("correct answer" , correctAnswer)
+            console.log("your answer" , answer)
+             if(answer < correctAnswer){
+                 console.log("emily is better than sean at league")
+                alert('Your answer is to low.  Try Again! You have ' + (numberOfAttempts - i) + ' attempts left');  
+            } else if (answer > correctAnswer){
+                alert('Your answer is to high.  Try Again! You have ' + (numberOfAttempts - i) + ' attempts left');
+            
+            } else {
+                alert('You got it right!');
+                correctAnswers++;
+                break;
+            }
+        
+        }
+        
+    
+    }
+}
+game()
+
+function colorGame(){
+    let answer = prompt('What is one of my favorite colors?').toLowerCase();
+    let correctAnswer = ['green', 'blue'];
+    let numberOfAttempts = 6 ;
+    for(let i = 1; i <= numberOfAttempts; i++){
+            console.log("correct answer" , correctAnswer)
+            console.log("your answer" , answer)
+             if(correctAnswer[0] == answer || 
+                correctAnswer[1] == answer){
+             alert('You got it right!');
+             correctAnswers++;
+                break;
+            } else { 
+            alert('Incorrect, You have ' + (numberOfAttempts - i) + ' attempts left');
+            }
+            answer = prompt('What are my favorite colors?').toLowerCase();
+        }
+}
+colorGame()
+
+ alert('You got ' + correctAnswers + ' out of 7 right!')
